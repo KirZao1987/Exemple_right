6c7d1db1cd5e1065b1c7ff96831d8467a25de3ca
Kirill <ganaboss66@gmail.com>
2023-03-06 15:12:38 +0300

    Пример5

---
+++ Example005_ifElse/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Write("Введите имя пользователя: ");
+string username = Console.ReadLine();
+
+if(username.ToLower() == "дина")
+{
+  Console.WriteLine("Привет малышка");
+}
+else
+{
+  Console.Write("И снова чижик, ");
+  Console.WriteLine(username);
+}
